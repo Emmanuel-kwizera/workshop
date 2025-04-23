1b01861d54f2be21f06cee67f16f555553c34cfa
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2025-04-23 16:35:03 +0200

    Add files via upload

---
+++ models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const userScheme = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    }
+})
+
+const User = mongoose.model('User', userScheme);
+
+export default User;
